4713ee59f1d84bafb1a6ba289c61d296d5dd6776
julen <julen@DESKTOP-A41EU1I>
2022-09-29 12:35:57 +0200

    prueba de commit clase

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Persona.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author julen
+ */
+public class Persona {
+    private int edad;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
